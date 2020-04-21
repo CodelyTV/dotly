4cd5d79298974a07f92c15f70c0be1f570771908
Rafa Gómez <rgomezcasas@gmail.com>
2020-04-21 16:55:41 +0200

    Apply symlinks on install

--- dotfiles_template/shell/init.sh
+++ dotfiles_template/shell/init.sh
@@ -1,10 +1,11 @@
+# This is a useful file to have the same aliases/functions in bash and zsh
 ulimit -n 200000
 ulimit -u 2048
 
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 
-# Register all aliases
+# Register custom aliases and functions
 for aliasToSource in "$DOTFILES_PATH/shell/_aliases/"*; do source "$aliasToSource"; done
-# Register all functions
+for exportToSource in "$DOTFILES_PATH/shell/_exports/"*; do source "$exportToSource"; done
 for functionToSource in "$DOTFILES_PATH/shell/_functions/"*; do source "$functionToSource"; done
--- scripts/dotfiles/utils/structure/shell/zsh/.zshenv
+++
@@ -1,9 +0,0 @@
-#
-# User configuration sourced by all invocations of the shell
-#
-
-# Define Zim location
-: ${ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim}
-
-for exportToSource in "$DOTFILES_PATH/shell/_exports/"*; do source "$exportToSource"; done
-
