44e5c7095b647c176e9d83e20de28617a7f59379
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-01 12:50:00 +0100

    Install always needed mac packages

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -4,18 +4,19 @@ install_macos_custom() {
   # Install brew if not installed
   if ! [ -x "$(command -v brew)" ]; then
     CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    export PATH="$PATH:/usr/local/bin"
-    mkdir -p "$HOME/bin"
-
-    brew install bash
-    brew install coreutils
-    brew install make
-    brew install gnu-sed
-    brew install findutils
-    brew install bat
-    brew install hyperfine
   fi
+
+  # Install needed packages
+  export PATH="$PATH:/usr/local/bin"
+  mkdir -p "$HOME/bin"
+
+  brew install bash
+  brew install coreutils
+  brew install make
+  brew install gnu-sed
+  brew install findutils
+  brew install bat
+  brew install hyperfine
 }
 
 install_linux_custom() {
