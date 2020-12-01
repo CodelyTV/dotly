62a04fd71bf946ab6dbda412761de9ed1bf0e916
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-01 11:25:26 +0100

    Install macOS custom packages
    Fixes #29

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -3,18 +3,8 @@
 install_macos_custom() {
   # Install brew if not installed
   if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-
-  # All apps (This line is 2 times because there are dependencies between brew cask and brew)
-  brew bundle --file="$DOTLY_PATH/mac/brew/Brewfile" || true
-  brew bundle --file="$DOTLY_PATH/mac/brew/Brewfile"
-
-  # Correct paths (so, we handle all with $PATH)
-  sudo truncate -s 0 /etc/paths
-
-  # Custom macOS "defaults"
-  sh "$DOTLY_PATH/mac/mac-os.sh"
 }
 
 install_linux_custom() {
