0fc39851ea458ac8c221584e1a150a137d9fca9c
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-06 14:34:26 +0100

    Add a script to import packages

--- scripts/package/utils/dump.sh
+++ scripts/package/utils/dump.sh
@@ -4,20 +4,38 @@ HOMEBREW_DUMP_FILE_PATH="$DOTFILES_PATH/os/mac/brew/Brewfile"
 PYTHON_DUMP_FILE_PATH="$DOTFILES_PATH/langs/python/requirements.txt"
 NPM_DUMP_FILE_PATH="$DOTFILES_PATH/langs/js/global_modules.txt"
 
-package::dump_brew() {
+package::brew_dump() {
   mkdir -p "$DOTFILES_PATH/os/mac/brew"
 
   brew bundle dump --file="$HOMEBREW_DUMP_FILE_PATH" --force
 }
 
-package::dump_python() {
-  mkdir -p "$DOTFILES_PATH/langs/python"
-
-  pip freeze >"$PYTHON_DUMP_FILE_PATH"
+package::brew_import() {
+  if [ -f "$HOMEBREW_DUMP_FILE_PATH" ]; then
+    brew bundle install --file="$HOMEBREW_DUMP_FILE_PATH"
+  fi
 }
 
-package::dump_npm() {
+package::python_dump() {
+  mkdir -p "$DOTFILES_PATH/langs/python"
+
+  pip3 freeze >"$PYTHON_DUMP_FILE_PATH"
+}
+
+package::python_import() {
+  if [ -f "$PYTHON_DUMP_FILE_PATH" ]; then
+    pip3 install -r "$PYTHON_DUMP_FILE_PATH"
+  fi
+}
+
+package::npm_dump() {
   mkdir -p "$DOTFILES_PATH/langs/js"
 
   ls -1 /usr/local/lib/node_modules | grep -v npm >"$NPM_DUMP_FILE_PATH"
+}
+
+package::npm_import() {
+  if [ -f "$NPM_DUMP_FILE_PATH" ]; then
+    xargs -I_ npm install -g "_" <"$NPM_DUMP_FILE_PATH"
+  fi
 }
