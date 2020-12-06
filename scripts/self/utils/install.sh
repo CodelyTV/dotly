c4585555a78b2b93c7187c588558cf38fdf0662d
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-06 10:41:15 +0100

    Improve installation output

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -1,22 +1,24 @@
 #!/bin/user/env bash
 
 install_macos_custom() {
-  # Install brew if not installed
-  if ! [ -x "$(command -v brew)" ]; then
-    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if ! platform::command_exists brew; then
+    output::error "brew not installed, installing"
+    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null 2>&1
   fi
 
   # Install needed packages
   export PATH="$PATH:/usr/local/bin"
   mkdir -p "$HOME/bin"
 
-  brew install bash
-  brew install coreutils
-  brew install make
-  brew install gnu-sed
-  brew install findutils
-  brew install bat
-  brew install hyperfine
+  output::answer "Installing needed gnu packages"
+
+  brew install bash >/dev/null 2>&1
+  brew install coreutils >/dev/null 2>&1
+  brew install make >/dev/null 2>&1
+  brew install gnu-sed >/dev/null 2>&1
+  brew install findutils >/dev/null 2>&1
+  brew install bat >/dev/null 2>&1
+  brew install hyperfine >/dev/null 2>&1
 }
 
 install_linux_custom() {
