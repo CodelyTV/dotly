08182425789c8fb746323b29b5017ac09cb4d30e
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-06 03:15:33 +0100

    Simplify dot command

--- scripts/core/_main.sh
+++ scripts/core/_main.sh
@@ -4,12 +4,12 @@ if ! ${DOT_MAIN_SOURCED:-false}; then
   source "$DOTLY_PATH/scripts/core/args.sh"
   source "$DOTLY_PATH/scripts/core/collections.sh"
   source "$DOTLY_PATH/scripts/core/documentation.sh"
+  source "$DOTLY_PATH/scripts/core/dot.sh"
   source "$DOTLY_PATH/scripts/core/git.sh"
   source "$DOTLY_PATH/scripts/core/log.sh"
   source "$DOTLY_PATH/scripts/core/platform.sh"
   source "$DOTLY_PATH/scripts/core/output.sh"
   source "$DOTLY_PATH/scripts/core/str.sh"
-  source "$DOTLY_PATH/scripts/core/paths.sh"
 
   readonly DOT_MAIN_SOURCED=true
 fi
