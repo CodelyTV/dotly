3861a9319d6deb59388d748e0612d4fbbe6faae2
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-06 01:57:19 +0100

    Use docpars in all scripts

--- scripts/core/_main.sh
+++ scripts/core/_main.sh
@@ -3,6 +3,7 @@
 if ! ${DOT_MAIN_SOURCED:-false}; then
   source "$DOTLY_PATH/scripts/core/args.sh"
   source "$DOTLY_PATH/scripts/core/collections.sh"
+  source "$DOTLY_PATH/scripts/core/documentation.sh"
   source "$DOTLY_PATH/scripts/core/log.sh"
   source "$DOTLY_PATH/scripts/core/platform.sh"
   source "$DOTLY_PATH/scripts/core/output.sh"
