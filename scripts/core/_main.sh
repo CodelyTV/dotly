700b518ebfc99be018429f61489d6b03affae315
Rafa Gómez <rgomezcasas@gmail.com>
2020-12-06 11:41:52 +0100

    Update _main.sh

--- scripts/core/_main.sh
+++ scripts/core/_main.sh
@@ -2,7 +2,7 @@
 
 if ! ${DOT_MAIN_SOURCED:-false}; then
   for file in $DOTLY_PATH/scripts/core/{args,collections,documentation,dot,git,log,platform,output,str}.sh; do
-	  source "$file";
+    source "$file";
   done;
   unset file;
 
