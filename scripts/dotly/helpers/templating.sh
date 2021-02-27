aeb64d69d53467e2702cc11b59ca1c8e51584ce5
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-02-27 17:33:27 +0100

    Now the description does not need to be between " (double quotes). For me makes sense to define the command name as "dot $context $script_name"

--- scripts/dotly/helpers/templating.sh
+++ scripts/dotly/helpers/templating.sh
@@ -2,12 +2,14 @@
 # from Rafa Gomez original dotly creator
 dotly::apply_templating() {
   local script_filepath="$1"
-  local script_author="$2"
-  local script_email="$3"
-  local script_description="$4"
+  local script_context="$2"
+  local script_author="$3"
+  local script_email="$4"
+  local script_description="${@:5}"
   
   sed -i -e "s|XXX_SCRIPT_DESCRIPTION_XXX|$script_description|g" "$script_filepath"
   sed -i -e "s|XXX_SCRIPT_AUTHOR_EMAIL_XXX|$script_email|g" "$script_filepath"
   sed -i -e "s|XXX_SCRIPT_AUTHOR_XXX|$script_author|g" "$script_filepath"
   sed -i -e "s|XXX_SCRIPT_NAME_XXX|$(basename $script_filepath)|g" "$script_filepath"
+  sed -i -e "s|XXX_SCRIPT_CONTEXT_XXX|$script_context|g" "$script_filepath"
 }
