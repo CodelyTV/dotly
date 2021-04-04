b053231e4c96271446808a51357958cf18cb17e2
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-04-04 16:22:28 +0200

    Fixed the use of shift when not necessary

--- scripts/core/files.sh
+++ scripts/core/files.sh
@@ -2,7 +2,7 @@
 
 files::check_if_path_is_older() {
   local path number_of period
-  path="$1"; shift
+  path="$1"
   number_of="${2:-0}"
   period="${3:-days}"
   [[ -e "$path" ]] && [[ $(date -r "$1" +%s) -lt $(date -d "now - $number_of $period" +%s) ]]
