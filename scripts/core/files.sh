ae4a4df3f335fef379a8ef46aabe5c939f7cf9d0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-04-04 16:16:31 +0200

    Fixed the use of files::check_if_path_is_older, refactored files::check_if_path_is_older to be more clear and clean in the usage

--- scripts/core/files.sh
+++ scripts/core/files.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 files::check_if_path_is_older() {
-  [[ $(date -r "$1" +%s) -lt $(date -d "now - $2 days" +%s) ]]
+  local path number_of period
+  path="$1"; shift
+  number_of="${2:-0}"
+  period="${3:-days}"
+  [[ -e "$path" ]] && [[ $(date -r "$1" +%s) -lt $(date -d "now - $number_of $period" +%s) ]]
 }
