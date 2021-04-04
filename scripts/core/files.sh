13b6516805255292227775d7bfdcac60ec8d2754
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-04-04 16:28:10 +0200

    Fixed the usage of variable name path inside 'files::check_if_path_is_older' that makes the function to crash

--- scripts/core/files.sh
+++ scripts/core/files.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 files::check_if_path_is_older() {
-  local path number_of period
-  path="$1"
+  local path_to_check number_of period
+  path_to_check="$1"
   number_of="${2:-0}"
   period="${3:-days}"
-  [[ -e "$path" ]] && [[ $(date -r "$1" +%s) -lt $(date -d "now - $number_of $period" +%s) ]]
+  [[ -e "$path_to_check" ]] && [[ $(date -r "$path_to_check" +%s) -lt $(date -d "now - $number_of $period" +%s) ]]
 }
