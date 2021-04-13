848379dee6d55383d3870f3e12f32e6d92852b85
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-04-13 14:55:49 +0200

    Added add file to private store

--- scripts/secrets/lib/secrets.sh
+++ scripts/secrets/lib/secrets.sh
@@ -4,25 +4,16 @@
 if ! ${DOTLY_SECRETS_MODULE_PATH:-false}; then
   readonly DOTLY_SECRETS_MODULE_PATH="modules/secrets"
 
-  if ! platform::command_exists yq; then
-    output::error "The command yq version 4 is needed, try by executing:"
-    output::answer "$DOTLY_PATH/bin/dot" package install yq
+  if ! platform::command_exists jq || ! platform::command_exists sponge; then
+    output::error "The commands 'jq' and 'sponge' are needed, try by executing:"
+    output::answer "$DOTLY_PATH/bin/dot" package install jq
+    output::answer "$DOTLY_PATH/bin/dot" package install sponge
     output::empty_line
-    output::yesno "Do you want to execute now and continue" &&\
-      "$DOTLY_PATH/bin/dot" package install yq || exit 1
+    output::yesno "Do you want to execute now and continue" && {
+        "$DOTLY_PATH/bin/dot" package install jq
+        "$DOTLY_PATH/bin/dot" package install sponge
+      } || exit 1
     output::empty_line
-  elif [[ $(platform::semver_compare 4.0.0 "$(yq --version | awk '{print $3}')") -gt 0 ]]; then
-    output::error "Wrong 'yq' version: There is needed at least version 4.0.0"
-    exit 1
-  fi
-
-  if [[ ! -f "$DOTFILES_PATH/shell/zsh/completions/_yq" ]]; then
-    yq shell-completion zsh > "$DOTFILES_PATH/shell/zsh/completions/_yq"
-    "$DOTLY_PATH/bin/dot" shell zsh reload_completions
-  fi
-
-  if [[ ! -f "$DOTFILES_PATH/shell/bash/completions/_yq" ]]; then
-    yq shell-completion bash > "$DOTFILES_PATH/shell/bash/completions/_yq"
   fi
 fi
 
@@ -57,10 +48,18 @@ secrets::var() {
   fi
 }
 
+secrets::remove_file() {
+  local start_dir
+  start_dir="$(pwd)"
+  cd "$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH" || return 1
+  git filter-branch --index-filter 'git rm -f --ignore-unmatch $(realpath --relative-to="$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH" "$1")' HEAD
+  cd "$start_dir" || return
+}
+
 secrets::var_delete() {
   local var_path
   var_path="$(secrets::var_exists "$1")"; shift
-  [[ -n "$var_path" ]] && rm -f "$var_path"
+  [[ -n "$var_path" ]] && secrets::remove_file "$var_path"
 }
 
 secrets::apply() {
@@ -100,34 +99,30 @@ secrets::revert() {
 }
 
 secrets::store() {
-  local secret_files_path item_symlink_path secret_link secrets_yaml
+  local secret_files_path item_symlink_path secret_link secrets_json
   item_symlink_path="$(secrets::relative_path "$1")"
-  secrets_yaml="$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/symlinks/secrets.yaml"
+  secrets_json="$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/symlinks/secrets.json"
 
-  if [[ -z "${2:-}" ]]; then
+  if [[ -n "$2" ]]; then
+    secret_files_path="$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/files/$2"
+    secret_link="\$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/files/$2/$(basename "$1")"
+  else
     secret_files_path="$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/files"
     secret_link="\$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/files/$(basename "$1")"
-  else
-    secret_files_path="$secret_files_path/$2"
-    secret_link="\$DOTFILES_PATH/$DOTLY_SECRETS_MODULE_PATH/files/$2/$(basename "$1")"
   fi
 
-  if [[ -d "$secret_files_path/$(basename "$1")" ]]; then
+  if [[ -e "$secret_files_path/$(basename "$1")" ]]; then
     output::error "Already exists a file with this name, try adding a different secrets relative path"
     return 1
   fi
 
   output::answer "Moving the file to your secrets path"
-  #mv "$1" "$secret_files_path/"
+  mkdir -p "$secret_files_path/"
+  mv -n "$1" "$secret_files_path/"
 
   output::answer "Linking your file"
-  #eval ln -s "$secret_files_path/$(basename "$item_symlink_path")" "$item_symlink_path"
+  eval ln -s "$secret_files_path/$(basename "$item_symlink_path")" "$item_symlink_path"
 
-  # Add to yaml secrets file
-  #TODO Test and if it fails fix this when link is not the firs element, we need to search firs wich is the good one to modify
-  #yq --yaml-output -r "select(.[].link != null) .[].link += {\"$item_symlink_path\": \"$secret_link\"}" "$secrets_yaml" > "$secrets_yaml.tmp"
-  yq ". | select(.[].link != null)" "$secrets_yaml"
-  #yq ".[] | select(.link != null) .link += {\"$item_symlink_path\": \"$secret_link\"}" "$secrets_yaml"
-  #rm -f "$secrets_yaml"
-  #mv "$secrets_yaml.tmp" "$secrets_yaml"
+  # Add to json secrets file
+  jq ".[1].link += {\"$item_symlink_path\": \"$secret_link\"}" "$secrets_json" | sponge "$secrets_json"
 }
