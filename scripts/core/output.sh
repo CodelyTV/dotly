cb8acbc1357c0c79338297501f471e140e5520e0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-05-08 21:29:28 +0200

    Added all features for this branch - Added core functions in output.sh: output::question_default, output::yesno - Fixed in core functions output.sh: output::question. This was adding a new line to the prompt that creates confusion. - Added export/import packages of package managers by given alias or hostname. - Added export/import mac defaults by given alias or hostname.

--- scripts/core/output.sh
+++ scripts/core/output.sh
@@ -14,11 +14,34 @@ output::error() { output::answer "${red}$1${normal}"; }
 output::solution() { output::answer "${green}$1${normal}"; }
 output::question() {
   if [ platform::is_macos ]; then
-    output::answer "🤔 $1: ";
+    echo -n " > 🤔 $1: ";
     read -r "$2";
   else
-    read -rp " $1: " "$2"
+    read -rp "🤔 $1: " "$2"
   fi
+}
+output::question_default() {
+  local question default_value var_name
+  question="$1"
+  default_value="$2"
+  var_name="$3"
+
+  output::question "$question? [$default_value]" "$var_name"
+  eval "$var_name=\"\${$var_name:-$default_value}\""
+}
+output::yesno() {
+  local question default PROMPT_REPLY values
+  question="$1"
+  default="${2:-Y}"
+
+  if [[ "$default" =~ ^[Yy] ]]; then
+    values="Y/n"
+  else
+    values="y/N"
+  fi
+
+  output::question "$question? [$values]" "PROMPT_REPLY"
+  [[ "${PROMPT_REPLY:-$default}" =~ ^[Yy] ]]
 }
 output::empty_line() { echo ''; }
 output::header() { output::empty_line; output::write "${bold_blue}---- $1 ----${normal}"; }
