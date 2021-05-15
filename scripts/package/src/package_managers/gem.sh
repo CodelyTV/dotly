1428cc6cd46c48f4420fe0076654548475f9f05e
Rafa Gómez <rgomezcasas@gmail.com>
2021-05-15 04:37:43 +0200

    feat: improve gem update output

---
+++ scripts/package/src/package_managers/gem.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+gem::update_all() {
+ outdated=$(gem outdated)
+
+  if [ -n "$outdated" ]; then
+    echo "$outdated" | while IFS= read -r outdated_app; do
+      package=$(echo "$outdated_app" | awk '{print $1}')
+      current_version=$(echo "$outdated_app" | awk '{print $2}' | sed 's/(//g')
+      new_version=$(echo "$outdated_app" | awk '{print $4}' | sed 's/)//g')
+
+      output::write "♦️  $package"
+      output::write "└ $current_version -> $new_version"
+      output::empty_line
+
+      gem update "$package" 2>&1 | log::file "Updating gem app: $package"
+    done
+  else
+    output::answer "Already up-to-date"
+  fi
+}
