b7844a5e4d0d8c7778b0a6eac2f281567bd6c9d4
Rafa Gómez <rgomezcasas@gmail.com>
2021-05-15 03:14:49 +0200

    feat: improve mas update output

--- scripts/core/output.sh
+++ scripts/core/output.sh
@@ -3,6 +3,7 @@
 red='\033[0;31m'
 green='\033[0;32m'
 bold_blue='\033[1m\033[34m'
+gray='\e[90m'
 normal='\033[0m'
 
 output::write() {
@@ -10,6 +11,7 @@ output::write() {
   echo -e "$text"
 }
 output::answer() { output::write " > $1"; }
+output::clarification() { output::write " ${gray}$1${normal}"; }
 output::error() { output::answer "${red}$1${normal}"; }
 output::solution() { output::answer "${green}$1${normal}"; }
 output::question() {
@@ -20,5 +22,11 @@ output::question() {
     read -rp " $1: " "$2"
   fi
 }
+
 output::empty_line() { echo ''; }
+
 output::header() { output::empty_line; output::write "${bold_blue}---- $1 ----${normal}"; }
+output::h1_without_margin() { output::write "${bold_blue}# $1${normal}"; }
+output::h1() { output::empty_line; output::h1_without_margin "$1"; }
+output::h2() { output::empty_line; output::write "${bold_blue}## $1${normal}"; }
+output::h3() { output::empty_line; output::write "${bold_blue}### $1${normal}"; }
