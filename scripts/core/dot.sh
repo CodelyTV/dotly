77ae4ce5183b3779d546920260c658075ecadaab
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-05-27 23:06:06 +0200

    Restored templating, dot and output from master to avoid merging problems

--- scripts/core/dot.sh
+++ scripts/core/dot.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPT_LOADED_LIBS=("${LOADED_LIBS[@]:-}")
-
 dot::list_contexts() {
   dotly_contexts=$(ls "$DOTLY_PATH/scripts")
   dotfiles_contexts=$(ls "$DOTFILES_PATH/scripts")
@@ -33,56 +31,4 @@ dot::list_scripts_path() {
   dotfiles_contexts=$(find "$DOTFILES_PATH/scripts" -maxdepth 2 -perm /+111 -type f)
 
   printf "%s\n%s" "$dotly_contexts" "$dotfiles_contexts" | sort -u
-}
-
-dot::get_script_path() {
-  echo "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-}
-
-dot::get_full_script_path() {
-  echo "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0")"
-}
-
-dot::get_script_src_path() {
-  local lib lib_path lib_paths lib_full_path
-  lib="${1:-}"
-  lib_full_path=""
-
-  if [[ -n "${lib:-}" ]]; then
-    lib_paths=()
-    if [[ -n "${2:-}" ]]; then
-      lib_paths+=("$DOTFILES_PATH/scripts/$2/src" "$DOTLY_PATH/scripts/$2/src" "$2")
-    else
-      lib_paths+=(
-        "$(dot::get_script_path)/src"
-      )
-    fi
-    
-    lib_paths+=(
-      "$DOTLY_PATH/scripts/core"
-      "."
-    )
-
-    for lib_path in "${lib_paths[@]}"; do
-      [[ -f "$lib_path/$lib" ]] &&\
-        lib_full_path="$lib_path/$lib" &&\
-        break
-
-      [[ -f "$lib_path/$lib.sh" ]] &&\
-        lib_full_path="$lib_path/$lib.sh" &&\
-        break
-    done
-
-    # Library loading
-    if [[ -n "${lib_full_path:-}" ]] && [[ -f "${lib_full_path:-}" ]]; then
-      ! [[ " ${LOADED_LIBS[@]} " =~ " ${lib_full_path:-} " ]] && . "$lib_full_path"
-      return 0
-    else
-      output::error "🚨 Library loading error with: \"${lib_full_path:-No lib path found}\""
-      exit 1
-    fi
-  fi
-  
-  # No arguments
-  return 1
 }
--- scripts/core/output.sh
+++ scripts/core/output.sh
@@ -3,20 +3,23 @@
 red='\033[0;31m'
 green='\033[0;32m'
 bold_blue='\033[1m\033[34m'
+gray='\e[90m'
 normal='\033[0m'
 
-platform::is_macos() {
-  [[ $(uname -s) == "Darwin" ]]
-}
-
 output::write() {
-  local -r text="${1:-}"
+  local -r text="${*:-}"
   echo -e "$text"
 }
-output::answer() { output::write " > $@"; }
-output::error() { output::answer "${red}$@${normal}"; }
-output::solution() { output::answer "${green}$@${normal}"; }
+output::answer() { output::write " > ${*:-}"; }
+output::clarification() {
+  with_code_parsed=$(echo "${*:-}" | awk "{ORS=(NR+1)%2==0?\"${green}\":RS}1" RS="\`" | awk "{ORS=NR%1==0?\"${normal}\":RS}1" RS="\`"| tr -d '\n')
+  output::write "$with_code_parsed";
+}
+output::error() { output::answer "${red}${*:-}${normal}"; }
+output::solution() { output::answer "${green}${*:-}${normal}"; }
 output::question() {
+  [[ $# -ne 2 ]] && return 1
+
   if [ platform::is_macos ]; then
     echo -n " > 🤔 $1: ";
     read -r "$2";
@@ -26,15 +29,21 @@ output::question() {
 }
 output::question_default() {
   local question default_value var_name
-  question="$1"
-  default_value="$2"
-  var_name="$3"
+
+  [[ $# -ne 3 ]] && return 1
+
+  question="${1:-}"
+  default_value="${2:-}"
+  var_name="${3:-}"
 
   output::question "$question? [$default_value]" "$var_name"
   eval "$var_name=\"\${$var_name:-$default_value}\""
 }
 output::yesno() {
   local question default PROMPT_REPLY values
+
+  [[ $# -eq 0 ]] && return 1
+
   question="$1"
   default="${2:-Y}"
 
@@ -47,5 +56,11 @@ output::yesno() {
   output::question "$question? [$values]" "PROMPT_REPLY"
   [[ "${PROMPT_REPLY:-$default}" =~ ^[Yy] ]]
 }
+
 output::empty_line() { echo ''; }
-output::header() { output::empty_line; output::write "${bold_blue}---- $1 ----${normal}"; }
+
+output::header() { output::empty_line; output::write "${bold_blue}---- ${*:-} ----${normal}"; }
+output::h1_without_margin() { output::write "${bold_blue}# ${*:-}${normal}"; }
+output::h1() { output::empty_line; output::h1_without_margin "${*:-}"; }
+output::h2() { output::empty_line; output::write "${bold_blue}## ${*:-}${normal}"; }
+output::h3() { output::empty_line; output::write "${bold_blue}### ${*:-}${normal}"; }
