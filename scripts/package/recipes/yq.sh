854c0c8948b05b3ace84afbdafa8fd9bb4b100f4
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-05-30 00:24:18 +0200

    Added yq receipe

--- scripts/package/recipes/yq.sh
+++ scripts/package/recipes/yq.sh
@@ -1,35 +1,20 @@
-#!/bin/user/env bash
+dot::get_script_src_path "yq.sh" "self"
 
-installer::get_arch() {
-  local architecture=""
-  case $(uname -m) in
-    i386|i686)
-      architecture="386"
-      ;;
-    x86_64)
-      architecture="amd64"
-      ;;
-    arm)
-      architecture="arm"
-      ;;
-  esac
-
-  echo "$architecture"
-}
-
-installer::get_os() {
-  uname | tr '[:upper:]' '[:lower:]'
-}
-
-installer::update_in_home() {
+yq::install() {
   local binary_name yq_path
+
   binary_name="yq_$(installer::get_os)_$(installer::get_arch)"
   yq_path="$HOME/bin/yq"
   output::error "yq not installed, installing"
 
+  "$DOTLY_PATH/bin/dot" package add yq --skip-recipe | log::file "Installing yq"
+  if package::command is_installed yq; then
+    return 0
+  fi
+
   if platform::command_exists curl; then
     {
-      curl --silent "https://github.com/mikefarah/yq/releases/latest/download/$binary_name" >| "$yq_path" 2>/dev/null &&\
+      curl "https://github.com/mikefarah/yq/releases/latest/download/$binary_name" -o "$yq_path" >/dev/null 2>&1 &&\
       chmod +x "$yq_path"
     } || {
       output::error "Could not install yq!"
@@ -46,4 +31,13 @@ installer::update_in_home() {
       exit 5
     }
   fi
+
+  if platform::command_exists go; then
+    GO111MODULE=on go get github.com/mikefarah/yq/v4 >/dev/null 2>&1 || {
+      output::error "Could not install yq!"
+      exit 5
+    }
+  fi
+
+  output::solution "yq successfully installed!"
 }
