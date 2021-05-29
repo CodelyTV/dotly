f64b01e83f55524620187d3423095b36d52c90a0
Rafa Gómez <rgomezcasas@gmail.com>
2021-05-29 14:06:26 +0200

    feat: add git-delta recipe (#159)

--- scripts/package/recipes/_registry.sh
+++ scripts/package/recipes/_registry.sh
@@ -1,5 +1,5 @@
 if ! ${DOT_REGISTRY_SOURCED:-false}; then
-  for file in $DOTLY_PATH/scripts/package/recipes/{docpars,cargo}.sh; do
+  for file in $DOTLY_PATH/scripts/package/recipes/{docpars,cargo,git-delta}.sh; do
     source "$file"
   done
   unset file
@@ -11,6 +11,7 @@ registry::install() {
   case "$1" in
   docpars) docpars::install ;;
   cargo) cargo::install ;;
+  git-delta) git-delta::install ;;
   *) return 1 ;;
   esac
 }
