1b4c4b0f2476be0a54bac3cf729678d5d5c8ae1b
Rafa Gómez <rgomezcasas@gmail.com>
2021-06-05 12:04:26 +0200

    feat: add script for static analysis bash files

--- scripts/core/log.sh
+++ scripts/core/log.sh
@@ -45,9 +45,9 @@ _log() {
   local template=$1
   shift
   if ${log_to_file:-false}; then
-    echoerr -e $(printf "$template" "$@") | tee -a "$DOT_LOG_FILE" >&2
+    echoerr -e "$(printf "$template" "$@")" | tee -a "$DOT_LOG_FILE" >&2
   else
-    echoerr -e $(printf "$template" "$@")
+    echoerr -e "$(printf "$template" "$@")"
   fi
 }
 
