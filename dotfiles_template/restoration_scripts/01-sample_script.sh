95ef21a56a6431b927bc1718ccd6aa7cf47da18e
Rafa Gómez <rgomezcasas@gmail.com>
2021-06-05 12:24:22 +0200

    refactor: change restore per restorer

---
+++ dotfiles_template/restoration_scripts/01-sample_script.sh
@@ -0,0 +1,4 @@
+echo "This is a script ran by the restoration process. You can add yours inside '$DOTFILES_PATH/restoration_scripts'"
+echo
+echo "Self install scripts must have chmod u+x to run"
+echo
