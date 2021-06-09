2fc3058accdd36ce50532b990ec7bb7d6c195da0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-06-09 10:51:12 +0200

    Added custom package recipes by user

--- scripts/package/recipes/_registry.sh
+++ scripts/package/recipes/_registry.sh
@@ -1,15 +1,28 @@
 registry::recipe_exists() {
   local -r recipe="${1:-}"
-  local -r recipe_path="$DOTLY_PATH/scripts/package/recipes/$recipe.sh"
-  [[ -z "$recipe" || ! -f "$recipe_path" ]] && return
+  local recipe_path recipe_paths=()
+
+  if [[ -n "${2:-}" ]]; then
+    recipe_paths+=("$2")
+  fi
+
+  recipe_paths+=(
+    "$DOTLY_PATH/scripts/package/recipes/${recipe}.sh"
+    "$DOTFILES_PATH/recipes/${recipe}.sh"
+  )
+  [[ -z "$recipe" ]] && return
   
+  for recipe_path in "${recipe_paths[@]}"; do
+    [[ -f "$recipe_path" ]] && break
+  done
+
   echo "$recipe_path"
 }
 
 registry::install() {
   local -r recipe="${1:-}"
   local -r install_command="${recipe}::install"
-  local -r recipe_path="$(registry::recipe_exists "$recipe" || echo -n "")"
+  local -r recipe_path="$(registry::recipe_exists "$recipe" "${2:-}" || echo -n "")"
   
   [[ -z "$recipe" || -z "$recipe_path" || ! -f "$recipe_path" ]] && return 1
 
@@ -26,7 +39,7 @@ registry::install() {
 registry::is_installed() {
   local -r recipe="${1:-}"
   local -r is_installed_command="${recipe}::is_installed"
-  local -r recipe_path="$(registry::recipe_exists "$recipe")"
+  local -r recipe_path="$(registry::recipe_exists "$recipe" "${2:-}")"
   [[ -z "$recipe" || -z "$recipe_path" || ! -f "$recipe_path" ]] && return 1
   dot::load_library "${recipe}.sh" "$(dirname "$recipe_path")"
 
