75c444eb12c8d5e5dfe472131a939266e62291cc
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-03 13:45:25 +0200

    Added gawk, grep to gnutools to be installed

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -23,9 +23,11 @@ install_macos_custom() {
   brew list bash || brew install bash | log::file "Installing brew bash"
   brew list zsh || brew install zsh | log::file "Installing brew zsh"
   brew list coreutils || brew install coreutils | log::file "Installing brew coreutils"
-  brew list make || brew install make | log::file "Installing brew make"
-  brew list gnu-sed || brew install gnu-sed | log::file "Installing brew gnu-sed"
   brew list findutils || brew install findutils | log::file "Installing brew findutils"
+  brew list gnu-sed || brew install gnu-sed | log::file "Installing brew gnu-sed"
+  brew list gawk || brew install gawk | log::file "Installing brew gawk"
+  brew list grep || brew install grep | log::file "Installing brew grep"
+  brew list make || brew install make | log::file "Installing brew make"
   brew list bat || brew install bat | log::file "Installing brew bat"
   brew list hyperfine || brew install hyperfine | log::file "Installing brew hyperfine"
 
