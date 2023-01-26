a3e57b66370b7c3fd86cd9e845e95f036d5611e5
Gilberto Olivas <32525535+GOI17@users.noreply.github.com>
2023-01-26 14:04:44 -0800

    fix: dot is not showing scripts (#220)
    * ISSUE: Fix dot display scripts
    
    * fix: linting & static analysis
    
    Co-authored-by: Rafa Gómez <rgomezcasas@gmail.com>

--- dotfiles_template/shell/exports.sh
+++ dotfiles_template/shell/exports.sh
@@ -27,7 +27,7 @@ export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
 # ------------------------------------------------------------------------------
 # Path - The higher it is, the more priority it has
 # ------------------------------------------------------------------------------
-path+=(
+path=(
 	"$HOME/bin"
 	"$DOTLY_PATH/bin"
 	"$DOTFILES_PATH/bin"
@@ -44,6 +44,7 @@ path+=(
 	"/usr/bin"
 	"/usr/sbin"
 	"/sbin"
+	"$path"
 )
 
 export path
