1c2c6abe7e93965272f23ef5b081811fb10cdc4d
Javier Ferrer González <javier.mailserio@gmail.com>
2023-08-17 23:58:00 +0200

    refactor: move default ruby macos installation to gem namespace

--- scripts/package/src/package_managers/gem.sh
+++ scripts/package/src/package_managers/gem.sh
@@ -1,3 +1,10 @@
+gem::is_macos_default() {
+	current_ruby_path=$(command -v ruby)
+	default_ruby_path="/usr/bin/ruby"
+
+	platform::is_macos && [[ $current_ruby_path == "$default_ruby_path" ]]
+}
+
 gem::update_all() {
 	outdated=$(gem outdated)
 
